21debdecb465c80c1d1369cbbe68c5144ad6da85
dassschaf <timmy@fruchtwein.net>
2018-10-13 23:34:55 +0200

    preparing for the basics
    however, is broken yet.

---
+++ core/include/mongodb.js
@@ -0,0 +1,22 @@
+//
+//  SchafControl
+//  MySQL Connection file
+//
+//  used to provide a working MySQL Object in any file via
+//  let MongoDB = require('./include/mongodb.js');
+//
+
+//-- require: node packages --//
+let mc = require('mongodb').MongoClient;
+
+//-- require: other files --//
+let settings = require('./settings.js');
+
+
+//-- make the database object --//
+mc.connect(settings.mongodb.url, {poolSize: 25}, (err, db) =>
+{
+    if (err) throw err;
+
+    return db;
+});
---
+++ core/index.js
@@ -0,0 +1,60 @@
+//
+//  SchafControl TrackMania Forever Server Controller.
+//
+//  see License in the Git repository.
+//
+
+//-- require: node modules --//
+let gbxremote = require('gbxremote');
+
+
+//-- require: other files --//
+let settings = require('./include/settings.js');
+//let database = require('./include/mongodb.js');
+
+
+//-- set up connection variables --//
+let server = gbxremote.createClient(settings.server.host, settings.server.port);
+
+
+//-- set up connection to TMF server --//
+server.on('connect', () =>
+{
+    server.query('Authenticate', [settings.server.login, settings.server.password]).then(result =>
+    {
+
+    }).catch(error =>
+    {
+        throw error;
+    });
+    server.query('EnableCallbacks', [true]).then(result =>
+    {
+
+    }).catch(error =>
+    {
+        throw error;
+    });
+});
+
+server.on('error', (error) =>
+{
+    console.log(error);
+});
+
+//-- wait for the callbacks --//
+server.on('TrackMania.PlayerChat', params =>
+{
+
+    server.query('ChatSendServerMessage', ["faggot!"]);
+
+});
+
+
+
+//-- reject utility --//
+
+function r (object)
+{
+    console.log(JSON.stringify(object));
+}
+
