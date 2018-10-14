c03797db9b933eea9952b2a6553909c0a62df2ec
dassschaf <timmy@fruchtwein.net>
2018-10-14 18:55:30 +0200

    getting things ready for the first test

---
+++ core/include/dictionary.js
@@ -0,0 +1,14 @@
+//
+//	SchafControl dictionary
+//
+//
+
+let separators =
+	{
+		status: '$0af- '
+	};
+
+module.exports.en =
+	{
+		joinmessage: separators.status + '$z$s $ff0%player%$z$s$fff joined the server!'
+	};
---
+++ core/include/utilities.js
@@ -0,0 +1,16 @@
+//
+// SchafControl Utility library
+//
+
+module.exports =
+	{
+		fill: (string, replacements) =>
+		{
+			Object.keys(replacements).forEach(element =>
+			{
+				string = string.replace()
+			})
+
+			return string;
+		}
+	};
