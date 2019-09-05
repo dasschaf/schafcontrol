366eb8f25976f4174890a2ee22e984af8fe95937
dassschaf <timmy@fruchtwein.net>
2019-09-05 13:18:13 +0200

    added sample settings file to adjust after reinstalling

---
+++ include/sample_settings.js
@@ -0,0 +1,58 @@
+//
+//  SchafControl settings file
+//
+//  it should be documented...
+//
+
+exports.mongodb =
+    {
+        // MongoDB settings
+        //
+        // url:
+        // MongoDB connection URI, refer to https://docs.mongodb.com/manual/reference/connection-string/
+        url: 'mongodb://localhost:27017',
+
+        // options:
+        // MongoDB options object
+        options:
+            {
+                poolSize: 10,
+                useNewUrlParser: true
+            },
+
+        // database:
+        db: ''
+
+    };
+
+exports.server =
+    {
+        // TrackMania server settings
+        //
+        // host:
+        // The machine your TM server runs on. This is classically localhost.
+        host: '127.0.0.1',
+
+        // port:
+        // The port your TrackMania server listens at. Usually this is 5001, but varies if you have multiple servers.
+        port: 5001,
+
+        // login:
+        // The login for the SuperAdmin authorization level.
+        login: 'SuperAdmin',
+
+        // password:
+        // That very password.
+        password: '',
+
+        // masterserver_login:
+        // The login of your server, which it is registered to at the Master server of TMF.
+        masterserver_login: ''
+    };
+
+exports.masteradmin =
+    {
+        login: '',
+        
+        title: ''
+    };
